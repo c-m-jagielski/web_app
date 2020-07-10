92d38428fab84cdd75070021a7e65aa08dcee57e
Chris <chrisjagielski@gmail.com>
2020-07-10 17:35:27 -0500

    second button to add 10 to the number

--- Hello.js
+++ Hello.js
@@ -1,5 +1,13 @@
 function add(a, b) {
-	// Print out 'Hello World'
-	console.log('Hello world!');
 	return a + b;
+}
+
+function add10(a) {
+	//TODO type checking
+	return a + 10;
+}
+
+function genRand() {
+	// Return a random number between 1 and 100
+	return Math.floor(Math.random() * 100 + 1);
 }
