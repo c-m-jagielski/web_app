cf72bfbf9d6613765cb56acbf402c21ac23de947
Chris Jagielski <chrisjagielski@Chriss-MacBook-Air.local>
2020-07-10 16:24:49 -0500

    first html alert

---
+++ Hello.js
@@ -0,0 +1,23 @@
+function add(a, b) {
+	// Print out 'Hello World'
+	console.log('Hello world!');
+	return a + b;
+}
+/*
+<!DOCTYPE HTML>
+<html>
+
+<body>
+
+  <p>Before the script...</p>
+
+  <script>
+    alert( 'Hello, world!' );
+  </script>
+
+  <p>...After the script.</p>
+
+</body>
+
+</html>
+*/
