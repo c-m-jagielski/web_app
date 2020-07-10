c7dd8b30988a6c9e4f24961e906839527777786f
Chris <chrisjagielski@gmail.com>
2020-07-10 18:17:11 -0500

    add debugging and indentations

--- Hello.js
+++ Hello.js
@@ -1,13 +1,24 @@
 function add(a, b) {
+	var bolo = a + b;
+	alert(bolo);
 	return a + b;
 }
 
 function add10(a) {
 	//TODO type checking
+	var bolo = a + 10;
+	alert(bolo);
 	return a + 10;
 }
 
 function genRand() {
 	// Return a random number between 1 and 100
-	return Math.floor(Math.random() * 100 + 1);
+	var myNum = Math.floor(Math.random() * 100 + 1);
+	alert("HI!");
+	alert(myNum);
+	return myNum;
+}
+
+function functionName( s ){
+	alert('Hello, ' + s + '!');
 }
