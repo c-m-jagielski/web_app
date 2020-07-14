c0bb9c5a21baecb0248bd0597e773181b829d1e4
Chris <chrisjagielski@gmail.com>
2020-07-14 16:12:24 -0500

    valid error checking for number inputs only

--- Hello.js
+++ Hello.js
@@ -6,8 +6,12 @@ function add(a, b) {
 
 function add10(a) {
 	//TODO type checking
-	var bolo = a + 10;
-	alert(a + ' + 10 = ' + bolo);
+	var bolo = Number(a) + 10;
+	if (isNaN(bolo)) {
+		alert('Warning: You did not enter a valid number!')
+	} else {
+		alert(a + ' + 10 = ' + bolo);
+	}
 	return a + 10;
 }
 
