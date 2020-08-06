ef8bd025fcc92c1b1ea824041975d5cab18fef05
Chris Jagielski <chrisjagielski@gmail.com>
2020-08-06 16:06:35 -0500

    add html to my simple php example; include chess library

---
+++ index_chess.php
@@ -0,0 +1,14 @@
+<?php 
+#echo "hello world ... welcome to my web page!\nToday is: ";
+#echo date('Y-m-d H:i:s');
+
+include('/home/pi/Documents/git/web_app/chess01.html');
+echo date('Y-m-d H:i:s');
+?>
+
+<script type="text/javascript">
+
+//alert('Would you like to play chess?');
+var board1 = Chessboard('board1', 'start');
+
+</script>
