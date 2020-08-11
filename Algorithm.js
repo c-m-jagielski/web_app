f1ae1ee1c9f78557e96a95acc252f09bf1657508
Chris <chrisjagielski@gmail.com>
2020-08-11 13:21:33 -0500

    give each square on the board a unique number (ID)

--- Algorithm.js
+++ Algorithm.js
@@ -6,6 +6,18 @@ class ChessAI {
   fen = null;
   pgn = null;
   current_turn = 'w';
+  current_board = [];  // Keep track of where every piece is
+
+  SQUARES = {
+      a8:   0, b8:   1, c8:   2, d8:   3, e8:   4, f8:   5, g8:   6, h8:   7,
+      a7:  16, b7:  17, c7:  18, d7:  19, e7:  20, f7:  21, g7:  22, h7:  23,
+      a6:  32, b6:  33, c6:  34, d6:  35, e6:  36, f6:  37, g6:  38, h6:  39,
+      a5:  48, b5:  49, c5:  50, d5:  51, e5:  52, f5:  53, g5:  54, h5:  55,
+      a4:  64, b4:  65, c4:  66, d4:  67, e4:  68, f4:  69, g4:  70, h4:  71,
+      a3:  80, b3:  81, c3:  82, d3:  83, e3:  84, f3:  85, g3:  86, h3:  87,
+      a2:  96, b2:  97, c2:  98, d2:  99, e2: 100, f2: 101, g2: 102, h2: 103,
+      a1: 112, b1: 113, c1: 114, d1: 115, e1: 116, f1: 117, g1: 118, h1: 119
+  };
 
   constructor() {
     // Nothing yet. This could initialize settings, such as the type of game you want to play.
@@ -17,7 +29,7 @@ class ChessAI {
   }
 
   is_check(quiet) {
-    //TODO determine status
+    //TODO determine status - if any possible move of the opponent can take your King
     var check = false;
     if (check && !quiet) {
       alert('Check!');
@@ -55,7 +67,13 @@ class ChessAI {
   }
 
   generate_moves() {
-    var allMoves = [{'from':'qq', 'to':'qqq'}]
+    var allMoves = [{'from':'qq', 'to':'qqq'}];
+
+    /*for(var i = SQUARES.a8; i <= SQUARES.h1; i++) {
+      // Check if there's something in this spot
+      ?
+    }*/
+
     return allMoves;
   }
 
@@ -64,13 +82,22 @@ class ChessAI {
     //alert(this_move.to);
 
     //TODO Is this a valid move? (return false if not)
+    // A valid move will be included in the moves generated
+    var is_valid = false;
+    var current_moves = this.generate_moves();
+    /*for (a_move in current_moves) { //this is not proper JS
+      if (a_move === this_move) {
+        is_valid = true;
+        break;
+      }
+    }*/
 
     //TODO check if Pawn needs promotion (to Queen)
     //TODO check for castling
 
     this.update_turn()
 
-    return true
+    return true //is_valid
   }
 
 }
