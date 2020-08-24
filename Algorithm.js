af6fbb083d8a3ea10aa183305fd6ea3a53107e9d
Chris <chrisjagielski@gmail.com>
2020-08-24 13:28:29 -0500

    enable debug #1

--- Algorithm.js
+++ Algorithm.js
@@ -80,6 +80,11 @@ class ChessAI {
     this.current_board = {};
   }
 
+  debug_start(debug_board) {
+    this.current_board = this.deepCopy(debug_board);
+    this.current_turn = this.deepCopy(this.WHITE);
+  }
+
   set_orientation(orientation) {
     this.human = orientation;
   }
@@ -800,9 +805,9 @@ function do_clear () {
 
 function do_debug1 () {
   var posi = {
-    d6: 'bK',
-    d4: 'wP',
-    e4: 'wK'
+    d3: 'wQ',
+    e7: 'bK',
+    e3: 'wK'
   };
   var config2 = {
     draggable: true,
@@ -813,7 +818,18 @@ function do_debug1 () {
   }
   board = Chessboard("customBoard", config2)
 
-  game.start()
+  var debug_board = {
+    a8: null, b8: null, c8: null, d8: null, e8: null, f8: null, g8: null, h8: null,
+    a7: null, b7: null, c7: null, d7: null, e7: 'bK', f7: null, g7: null, h7: null,
+    a6: null, b6: null, c6: null, d6: null, e6: null, f6: null, g6: null, h6: null,
+    a5: null, b5: null, c5: null, d5: null, e5: null, f5: null, g5: null, h5: null,
+    a4: null, b4: null, c4: null, d4: null, e4: null, f4: null, g4: null, h4: null,
+    a3: null, b3: null, c3: null, d3: 'wQ', e3: 'wK', f3: null, g3: null, h3: null,
+    a2: null, b2: null, c2: null, d2: null, e2: null, f2: null, g2: null, h2: null,
+    a1: null, b1: null, c1: null, d1: null, e1: null, f1: null, g1: null, h1: null
+  };
+
+  game.debug_start(debug_board)
   $status.html("This is debug #1...")
 }
 
