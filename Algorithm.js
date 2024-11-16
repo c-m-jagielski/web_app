6379c1b498c02e9948b1b1a4c31f8e5919cdd4c2
Chris <chrisjagielski@gmail.com>
2024-11-15 20:55:12 -0600

    debug5 board adjustments

--- Algorithm.js
+++ Algorithm.js
@@ -1452,8 +1452,10 @@ function do_debug5 () {
   console.log('+++++++++++++++++++++++\n  User clicked Debug5 \n+++++++++++++++++++++++')
   var posi = {
     a3: 'bR',
+    c1: 'bQ',
     e5: 'bK',
     e2: 'wK',
+    g5: 'bR',
     h5: 'wP',
     h6: 'bP'
   };
@@ -1470,11 +1472,11 @@ function do_debug5 () {
     a8: null, b8: null, c8: null, d8: null, e8: null, f8: null, g8: null, h8: null,
     a7: null, b7: null, c7: null, d7: null, e7: null, f7: null, g7: null, h7: null,
     a6: null, b6: null, c6: null, d6: null, e6: null, f6: null, g6: null, h6: 'bP',
-    a5: null, b5: null, c5: null, d5: null, e5: 'bK', f5: null, g5: null, h5: 'wP',
+    a5: null, b5: null, c5: null, d5: 'bR', e5: null, f5: null, g5: 'bK', h5: 'wP',
     a4: null, b4: null, c4: null, d4: null, e4: null, f4: null, g4: null, h4: null,
     a3: 'bR', b3: null, c3: null, d3: null, e3: null, f3: null, g3: null, h3: null,
     a2: null, b2: null, c2: null, d2: null, e2: 'wK', f2: null, g2: null, h2: null,
-    a1: null, b1: null, c1: null, d1: null, e1: null, f1: null, g1: null, h1: null
+    a1: null, b1: null, c1: 'bQ', d1: null, e1: null, f1: null, g1: null, h1: null
   };
 
   game.debug_start(debug_board)
