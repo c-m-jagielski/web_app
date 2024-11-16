cb5d3677b7719b16014a0c03521786c35ef4de0f
Chris <chrisjagielski@gmail.com>
2024-11-15 20:58:42 -0600

    fix a typo in debug5

--- Algorithm.js
+++ Algorithm.js
@@ -1472,7 +1472,7 @@ function do_debug5 () {
     a8: null, b8: null, c8: null, d8: null, e8: null, f8: null, g8: null, h8: null,
     a7: null, b7: null, c7: null, d7: null, e7: null, f7: null, g7: null, h7: null,
     a6: null, b6: null, c6: null, d6: null, e6: null, f6: null, g6: null, h6: 'bP',
-    a5: null, b5: null, c5: null, d5: 'bR', e5: null, f5: null, g5: 'bK', h5: 'wP',
+    a5: null, b5: null, c5: null, d5: null, e5: 'bK', f5: null, g5: 'bR', h5: 'wP',
     a4: null, b4: null, c4: null, d4: null, e4: null, f4: null, g4: null, h4: null,
     a3: 'bR', b3: null, c3: null, d3: null, e3: null, f3: null, g3: null, h3: null,
     a2: null, b2: null, c2: null, d2: null, e2: 'wK', f2: null, g2: null, h2: null,
